8b25b98dba36396eb2aa75893ff9d32f72afac9e
Ezequiel <zequielzico@gmail.com>
2024-12-16 12:48:03 +0000

    init project

---
+++ headers/minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_string.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edos-san <edos-san@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 20:38:14 by edos-san          #+#    #+#             */
+/*   Updated: 2022/10/24 12:40:49 by edos-san         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ft_string.h"
+#include "ArrayList.h"
+#include <stdbool.h>
+# include <errno.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <termios.h>
+
+
+
+#endif
