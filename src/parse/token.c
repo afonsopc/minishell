6dc0ec5a07a7b72221dae1352b8568069131888f
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2025-03-13 17:09:55 +0000

    Add changes

--- src/parse/token.c
+++ src/parse/token.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   token.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afpachec <afpachec@42.fr>                  +#+  +:+       +#+        */
+/*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/18 11:32:12 by edos-san          #+#    #+#             */
-/*   Updated: 2024/12/23 13:59:43 by afpachec         ###   ########.fr       */
+/*   Created: 2025/02/10 11:32:12 by paude-so          #+#    #+#             */
+/*   Updated: 2025/03/13 14:55:19 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
+
+static void	set_token_type(t_token *t, char *type)
+{
+	if (str().equals("|", type))
+	{
+		t->type = PIPE;
+		t->balancing = 1;
+	}
+	else if (str().equals("||", type))
+	{
+		t->type = OR;
+		t->balancing = 1;
+	}
+	else if (str().equals("&&", type))
+	{
+		t->type = AND;
+		t->balancing = 1;
+	}
+	else
+		t->type = CMD;
+}
 
 t_token	*new_token(char *type, t_cmd *cmd)
 {
@@ -21,56 +42,10 @@ t_token	*new_token(char *type, t_cmd *cmd)
 	t = ft_calloc(sizeof(t_token));
 	if (!t)
 		return (NULL);
-	if (str().equals("|", type))
-		t->balancing = 1;
-	else if (str().equals("||", type))
-		t->balancing = 1;
-	else if (str().equals("&&", type))
-		t->balancing = 1;
 	t->cmd = cmd;
-	if (str().equals("|", type))
-		t->type = PIPE;
-	else if (str().equals("||", type))
-		t->type = OR;
-	else if (str().equals("&&", type))
-		t->type = AND;
-	else
-		t->type = CMD;
+	set_token_type(t, type);
 	t->pid = 0;
 	return (t);
-}
-
-static t_token	*swap(t_token *s1, t_token *s2)
-{
-	t_token	tmp1;
-
-	tmp1 = (*s1);
-	s1->type = s2->type;
-	s1->cmd = s2->cmd;
-	s1->left = s2->left;
-	s1->right = s2->right;
-	s1->balancing = s2->balancing;
-	s2->type = tmp1.type;
-	s2->cmd = tmp1.cmd;
-	s2->left = tmp1.left;
-	s2->right = tmp1.right;
-	s2->balancing = tmp1.balancing;
-	return (s1);
-}
-
-t_token	*balancing(t_token	*head, t_token *new)
-{
-	if (!head)
-		return (new);
-	if (new->balancing > head->balancing)
-		swap(head, new);
-	if (head->left == NULL)
-		head->left = new;
-	else if (head->right == NULL)
-		head->right = new;
-	else
-		balancing(head->right, new);
-	return (head);
 }
 
 bool	free_token(t_token *token)
@@ -81,6 +56,5 @@ bool	free_token(t_token *token)
 	free_token(token->left);
 	free_token(token->right);
 	free(token);
-	terminal()->token = NULL;
 	return (true);
 }
