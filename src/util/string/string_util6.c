8054442d5c4491afec23ab198e6b33b00d662c59
Paulo Victor <pv.35@hotmail.com>
2025-03-17 13:00:43 +0000

    added ft_strlcat

--- src/util/string/string_util6.c
+++ src/util/string/string_util6.c
@@ -6,7 +6,7 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 23:40:35 by afpachec          #+#    #+#             */
-/*   Updated: 2025/03/13 19:37:34 by paude-so         ###   ########.fr       */
+/*   Updated: 2025/03/17 12:59:28 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,27 @@ int	__starts_with(const char *str, const char *prefix)
 	if (prefix_len > str_len)
 		return (0);
 	return (__equals_n(str, prefix, prefix_len));
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	dst_len;
+	size_t	src_len;
+
+	i = 0;
+	while (dst[i++])
+		dst_len++;
+	while (src[i++])
+		src_len++;
+	if (size <= dst_len)
+		return (size + src_len);
+	i = 0;
+	while (src[i] && dst_len + i < size - 1)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (dst_len + src_len);
 }
