c80b10aed2559cff9969b4686a8473631ba981c7
Paulo Victor <pv.35@hotmail.com>
2025-03-17 13:49:03 +0000

    trying to implement quote parsing

---
+++ src/parse/var_expanding.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   var_expanding.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 13:05:50 by paude-so          #+#    #+#             */
+/*   Updated: 2025/03/17 13:48:41 by paude-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+char	*expand_exit_status(void)
+{
+	return (str().itoa(terminal()->status));
+}
+
+char	*expand_env_variable(char *var_name)
+{
+	char	*value;
+
+	value = hashmap(terminal()->env)->get_key(var_name);
+	if (value)
+		return (str().copy(value));
+	return (str().copy(""));
+}
+
+int	copy_variable_name(char *dest, char *src, int i)
+{
+	int	k;
+
+	k = 0;
+	while (src[i] && ((str().isalnum)(src[i]) || src[i] == '_'))
+		dest[k++] = src[i++];
+	dest[k] = '\0';
+	return (i);
+}
+
+char	*expand_arg(char *arg, int quoted)
+{
+	char	*result;
+	char	*value;
+	char	var_name[256];
+	int		i;
+	int		j;
+
+	result = ft_calloc(1000);
+	if (!result)
+		return (NULL);
+	if (quoted)
+		i = 3;
+	else
+		i = 0;
+	j = 0;
+	while (arg[i])
+	{
+		if (arg[i] == '$' && arg[i + 1])
+		{
+			i++;
+			ft_memset(var_name, 0, 256);
+			if (arg[i] == '?')
+			{
+				value = expand_exit_status();
+				i++;
+			}
+			else
+			{
+				i = copy_variable_name(var_name, arg, i);
+				value = expand_env_variable(var_name);
+			}
+			if (value)
+			{
+				ft_strlcat(result + j, value, 1000 - j);
+				j += str().size(value);
+				free(value);
+			}
+		}
+		else
+			result[j++] = arg[i++];
+	}
+	return (result);
+}
+
+char	*process_arg_expansion(char *arg)
+{
+	if (!arg || !*arg)
+		return (str().copy(""));
+	if (str().equals(arg, "$?"))
+		return (expand_exit_status());
+	if (str().starts_with(arg, "sq:"))
+		return (str().copy(arg));
+	if (str().starts_with(arg, "dq:"))
+		return (expand_arg(arg, 1));
+	if (str().contains(arg, "$"))
+		return (expand_arg(arg, 0));
+	return (str().copy(arg));
+}
--- src/util/string/string_util6.c
+++ src/util/string/string_util6.c
@@ -6,15 +6,15 @@
 /*   By: paude-so <paude-so@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 23:40:35 by afpachec          #+#    #+#             */
-/*   Updated: 2025/03/17 12:59:28 by paude-so         ###   ########.fr       */
+/*   Updated: 2025/03/17 13:32:10 by paude-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_string.h>
-#include <ft_util.h>
-#include <minishell.h>
-#include <stdio.h>
 #include <string_util.h>
+#include <stdio.h>
+#include <ft_util.h>
+#include <stdio.h>
 
 int	__isalpha(char c)
 {
@@ -53,32 +53,9 @@ int	__starts_with(const char *str, const char *prefix)
 
 	if (!str || !prefix)
 		return (0);
-	str_len = ft_size(str);
-	prefix_len = ft_size(prefix);
+	str_len = ft_strlen(str);
+	prefix_len = ft_strlen(prefix);
 	if (prefix_len > str_len)
 		return (0);
 	return (__equals_n(str, prefix, prefix_len));
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{
-	size_t	i;
-	size_t	dst_len;
-	size_t	src_len;
-
-	i = 0;
-	while (dst[i++])
-		dst_len++;
-	while (src[i++])
-		src_len++;
-	if (size <= dst_len)
-		return (size + src_len);
-	i = 0;
-	while (src[i] && dst_len + i < size - 1)
-	{
-		dst[dst_len + i] = src[i];
-		i++;
-	}
-	dst[dst_len + i] = '\0';
-	return (dst_len + src_len);
 }
