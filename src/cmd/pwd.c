45a8870f95d9d24b28bdc3cd31d80c4e02ac2346
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2025-04-04 00:35:36 +0100

    fix leak in pwd

--- src/cmd/pwd.c
+++ src/cmd/pwd.c
@@ -6,7 +6,7 @@
 /*   By: afpachec <afpachec@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 18:03:21 by afpachec          #+#    #+#             */
-/*   Updated: 2025/04/02 20:42:17 by afpachec         ###   ########.fr       */
+/*   Updated: 2025/04/04 00:26:40 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 
 pid_t	execute_pwd(t_cmd *cmd)
 {
-	ft_fputstr(cmd->out, get_cwd());
+	char	*pwd;
+
+	pwd = get_cwd();
+	ft_fputstr(cmd->out, pwd);
+	free(pwd);
 	ft_fputstr(cmd->out, "\n");
 	terminal()->status = 0;
 	ft_close2(cmd->in, cmd->out);
